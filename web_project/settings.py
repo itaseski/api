e9b2c2496c192e5b291870f03084be78b2169f15
Ilija Taseski <itaseski@t.mk>
2022-10-04 10:28:08 +0200

    api basic

--- web_project/settings.py
+++ web_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'movies.apps.MoviesConfig',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- web_project/urls.py
+++ web_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('movie/', include('movies.urls')),
+    path('movie/', include('movies.api.urls')),
 ]
