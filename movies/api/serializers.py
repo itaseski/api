d6c6043b1855cfbb2856f0ce325a58c9c88b46ef
Ilija Taseski <itaseski@t.mk>
2022-10-04 14:27:34 +0200

    validation

--- movies/api/serializers.py
+++ movies/api/serializers.py
@@ -1,9 +1,20 @@
+from wsgiref.validate import validator
 from rest_framework import serializers
 from movies.models import Movie
 
+import re
+
+def check_cyrillic(value):
+    """
+        Check if a string contains cyrillic characters
+    """
+    if bool(re.search('[\u0400-\u04FF]', value)):
+       raise serializers.ValidationError("Name contains cyrillic character!") 
+
+
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
+    name = serializers.CharField(validators=[check_cyrillic])
     description = serializers.CharField()
     active = serializers.BooleanField()
 
@@ -22,3 +33,21 @@ class MovieSerializer(serializers.Serializer):
         instance.active = validated_data.get('active', instance.active)
         instance.save()
         return instance
+
+
+    def validate(self, data):
+        """
+        Check that start is before finish.
+        """
+        if data['name'] == data['description']:
+            raise serializers.ValidationError("Name and description can't be the same!")
+        return data
+
+
+    def validate_name(self, value):
+        """
+        Check that the movie name is too short.
+        """
+        if len(value) < 2:
+            raise serializers.ValidationError("Name is too short!")
+        return value
